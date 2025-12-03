40061b21659fe1bc7df2a97e3123326b56a115eb
Nahom Garefo <harobina28@gmail.com>
2025-12-03 22:04:44 +0300

    add excute and capture function to pipe data from parent process to child process

--- src/helper.h
+++ src/helper.h
@@ -17,9 +17,12 @@ bool changeDirectory(const std::string& pathStr);
 bool checkBuiltin(std::string command);
 bool checkPath(std::string& output, const std::string& query);
 void concatenateString(std::string& output, std::vector<std::string>& arr, size_t start, size_t end);
-void excuteProgram(std::vector<std::string>& parsed_args);
+void excuteProgram(std::vector<std::string>& parsed_args, size_t end);
 bool isEscape(QUOTE_STATE state, char c);
 void pwd(std::string& output);
 void prepareArgsExternal(std::string& output, std::vector<std::string>& arr, int index);
 RedirectOutput checkRedirection(std::vector<std::string>& argList);
 void writeToFile(std::string& content, std::string& fileName);
+int excuteAndCapture(std::string& output,
+                    size_t end,
+                     std::vector<std::string>& args);
